79597666ce7a3eeb5e9083970d5d8d5843a20306
zpbc007 <719197352@qq.com>
2024-07-10 21:47:50 +0800

    fix: indent button

--- src/index.js
+++ src/index.js
@@ -1,34 +1,69 @@
 import { addEventListener } from "./bridge";
 
 const options = {
-  modules: {},
+  modules: {
+    toolbar: false,
+  },
   theme: "snow",
 };
 const quill = new Quill("#editor", options);
 
-function isFeatureActive(type) {
+function getSelectionFormats() {
   const range = quill.getSelection();
-  const formats = range == null ? {} : quill.getFormat(range);
+  return range == null ? {} : quill.getFormat(range);
+}
+
+function isFormatActive(type) {
+  const formats = getSelectionFormats();
   return formats[type] != null;
 }
 
 // 加粗操作
 addEventListener("toolbar.boldButtonTapped", () => {
-  const isActive = isFeatureActive("bold");
+  const isActive = isFormatActive("bold");
 
   quill.format("bold", !isActive);
 });
 
 // 有序列表操作
 addEventListener("toolbar.numberListButtonTapped", () => {
-  const isActive = isFeatureActive("list");
+  const formats = getSelectionFormats();
 
-  quill.format("list", isActive ? false : "ordered");
+  quill.format("list", formats.list === "ordered" ? false : "ordered");
 });
 
 // 无序列表操作
 addEventListener("toolbar.dashListButtonTapped", () => {
-  const isActive = isFeatureActive("list");
+  const formats = getSelectionFormats();
 
-  quill.format("list", isActive ? false : "bullet");
+  quill.format("list", formats.list === "bullet" ? false : "bullet");
+});
+
+// 勾选列表操作
+addEventListener("toolbar.checkBoxListButtonTapped", () => {
+  const formats = getSelectionFormats();
+
+  if (formats.list === "checked" || formats.list === "unchecked") {
+    quill.format("list", false);
+  } else {
+    quill.format("list", "unchecked");
+  }
+});
+
+function handleIndentChange(modifier) {
+  const formats = getSelectionFormats();
+  const indent = parseInt(formats.indent || 0, 10);
+  modifier *= formats.direction === "rtl" ? -1 : 1;
+
+  quill.format("indent", indent + modifier);
+}
+
+// 增加缩进操作
+addEventListener("toolbar.increaseIndentButtonTapped", () => {
+  handleIndentChange(1);
+});
+
+// 减少缩进操作
+addEventListener("toolbar.decreaseIndentButtonTapped", () => {
+  handleIndentChange(-1);
 });
