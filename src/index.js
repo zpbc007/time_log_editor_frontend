ad4ce04960f46b112062e42861a4edb5e5113c9a
zpbc007 <719197352@qq.com>
2024-07-10 18:09:48 +0800

    feat: 加粗操作

--- src/index.js
+++ src/index.js
@@ -8,5 +8,9 @@ const quill = new Quill("#editor", options);
 
 // 加粗操作
 addEventListener("toolbar.boldButtonTapped", () => {
-  console.log("toolbar.bold.tapped!!!");
+  const range = quill.getSelection();
+  const formats = range == null ? {} : quill.getFormat(range);
+  const isActive = formats["bold"] != null;
+
+  quill.format("bold", !isActive);
 });
