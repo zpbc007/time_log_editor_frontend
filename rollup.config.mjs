51de02b1c2075ca09caab53c90f0cba0ed638057
zpbc007 <719197352@qq.com>
2024-10-23 12:20:26 +0800

    feat: 修改 checkbox 样式

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,8 @@
 import del from "rollup-plugin-delete";
 import terser from "@rollup/plugin-terser";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import copy from "rollup-plugin-copy";
+import CleanCss from "clean-css";
 
 export default {
   input: "src/index.js",
@@ -8,6 +10,7 @@ export default {
     {
       file: "dist/bundle.js",
       format: "iife",
+      plugins: [],
     },
     {
       file: "dist/bundle.min.js",
@@ -15,5 +18,17 @@ export default {
       plugins: [terser()],
     },
   ],
-  plugins: [del({ targets: "dist/*" }), nodeResolve()],
+  plugins: [
+    del({ targets: "dist/*" }),
+    nodeResolve(),
+    copy({
+      targets: [
+        {
+          src: "./public/quill.snow.css",
+          dest: "dist/style",
+          transform: (contents) => new CleanCss().minify(contents).styles,
+        },
+      ],
+    }),
+  ],
 };
